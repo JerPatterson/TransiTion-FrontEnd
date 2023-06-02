9a191567851ec5d1429ed209b6ffad22abfce28d
JerPatterson <jeremie.patterson@polymtl.ca>
2023-06-02 10:07:16 -0400

    add a communication service for the firestore db

---
+++ src/app/services/communication.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { getFirestore, Firestore, collection, getDocs } from 'firebase/firestore';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CommunicationService {
+    private app: FirebaseApp;
+    private db: Firestore;
+
+    constructor() {
+        const firebaseConfig = {
+            apiKey: "AIzaSyDg8LZ-iLsQsdOcpbt7-x4140paDko0cQg",
+            authDomain: "transit-d9a47.firebaseapp.com",
+            projectId: "transit-d9a47",
+            storageBucket: "transit-d9a47.appspot.com",
+            messagingSenderId: "819307086539",
+            appId: "1:819307086539:web:6d88ec269b678cc4a0114e",
+            measurementId: "G-6YK9RSL08K"
+        };
+    
+        this.app = initializeApp(firebaseConfig);
+        this.db = getFirestore(this.app);
+    }
+
+    async getRoutesFromAgency(agencyId: string) {
+        return this.getCollectionFromAgency(agencyId, 'routes');
+    }
+
+    private async getCollectionFromAgency(agencyId: string, collectionId: string) {
+        return getDocs(collection(this.db, 'agencies', agencyId, collectionId));
+    }
+}
