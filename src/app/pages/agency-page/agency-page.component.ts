a1c1f062cf590a84934c2da957509b41d0abe122
JerPatterson <jeremie.patterson@polymtl.ca>
2023-06-15 19:50:13 -0400

    enable stm agency

--- src/app/pages/agency-page/agency-page.component.ts
+++ src/app/pages/agency-page/agency-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-agency-page',
-  templateUrl: './agency-page.component.html',
-  styleUrls: ['./agency-page.component.css']
+    selector: 'app-agency-page',
+    templateUrl: './agency-page.component.html',
+    styleUrls: ['./agency-page.component.css']
 })
 export class AgencyPageComponent {
+    agency: string | null;
 
+    constructor(private route: ActivatedRoute) {
+        this.agency = this.route.snapshot.paramMap.get('agency-name');
+    }
 }
