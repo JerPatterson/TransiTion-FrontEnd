de1a2d051b2b08f46e3e4d375cab2ac50a63b512
JerPatterson <jeremie.patterson@polymtl.ca>
2023-06-30 09:41:18 -0400

    remove firebase config as it is not useful as it stands

--- src/app/services/static/static-data.service.ts
+++ src/app/services/static/static-data.service.ts
@@ -1,30 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AgencyDto, RouteDto, ShapeDto, StopDto, TimeDto, TripDto } from '@app/utils/dtos';
 import { SERVER_URL } from '@app/utils/env';
-import { FirebaseApp, initializeApp } from 'firebase/app';
-import { getFirestore, Firestore, doc, collection, getDoc, DocumentSnapshot, DocumentData } from 'firebase/firestore';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StaticDataService {
-    private app: FirebaseApp;
-    private db: Firestore;
-
-    constructor() {
-        const firebaseConfig = {
-            apiKey: "AIzaSyDg8LZ-iLsQsdOcpbt7-x4140paDko0cQg",
-            authDomain: "transit-d9a47.firebaseapp.com",
-            projectId: "transit-d9a47",
-            storageBucket: "transit-d9a47.appspot.com",
-            messagingSenderId: "819307086539",
-            appId: "1:819307086539:web:6d88ec269b678cc4a0114e",
-            measurementId: "G-6YK9RSL08K"
-        };
-    
-        this.app = initializeApp(firebaseConfig);
-        this.db = getFirestore(this.app);
-    }
+    constructor() {}
 
     async getAgencies(): Promise<AgencyDto[]> {
         const res = await fetch(`${SERVER_URL}/agencies`);
@@ -78,25 +60,11 @@ export class StaticDataService {
 
     async getTimesFromStop(agencyId: string, stopId: string): Promise<TimeDto[]> {
         const res = await fetch(`${SERVER_URL}/times/stop/today/${agencyId}/${stopId}`);
-        return await res.json();
+        return res.json();
     }
 
     async getTimesFromStopOfRoute(agencyId: string, routeId: string, stopId: string): Promise<TimeDto[]> {
         const res = await fetch(`${SERVER_URL}/times/route/stop/today/${agencyId}/${routeId}/${stopId}`);
-        return await res.json();
-    }
-
-    async getArrayFromDocument(agencyId: string, documentId: string): Promise<any[]> {
-        const storedContent = sessionStorage.getItem(`${agencyId}/${documentId}`);
-        if (storedContent) return JSON.parse(storedContent) as any[];
-
-        const content = (await this.getDocumentFromAgency(agencyId, documentId)).data()?.arr as any[];
-        sessionStorage.setItem(`${agencyId}/${documentId}`, JSON.stringify(content));
-
-        return content;
-    }
-
-    async getDocumentFromAgency(agencyId: string, documentId: string): Promise<DocumentSnapshot<DocumentData>> {
-        return getDoc(doc(collection(this.db, agencyId), documentId));
+        return res.json();
     }
 }
