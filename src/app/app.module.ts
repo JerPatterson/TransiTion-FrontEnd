21f1f9c4ee1cba818e84a17cf98fe32db27d3096
JerPatterson <jeremie.patterson@polymtl.ca>
2023-06-30 16:27:56 -0400

    route type icon color change via text color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from '@app/components/app/app.component';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AgencyListComponent } from '@app/components/agency-list/agency-list.component';
