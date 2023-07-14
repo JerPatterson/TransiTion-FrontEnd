c5f46ab7d49713b3a18d58607187c6818c45b079
JerPatterson <jeremie.patterson@polymtl.ca>
2023-07-13 20:52:53 -0400

    sketch of what it could look like by using canvas based markers

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -7,11 +7,11 @@ export const SECONDS_IN_DAY = 86400;
 export const ACCEPTABLE_DELAY_IN_SEC = 20;
 
 export const PARAM_SEPARATOR = '/';
-export const DEFAULT_ICON_COLOR = '#000000';
+export const DEFAULT_ICON_COLOR = '%23000000';
 export const DEFAULT_SHAPE_COLOR = '#000000';
-export const DEFAULT_BACKGROUND_COLOR = '#f0f8ff';
-export const OLD_VEHICLE_COLOR = '#c1cacf';
-export const OLD_VEHICLE_BACKGROUND_COLOR = '#939b9f';
+export const DEFAULT_BACKGROUND_COLOR = '%23f0f8ff';
+export const OLD_VEHICLE_COLOR = '%23c1cacf';
+export const OLD_VEHICLE_BACKGROUND_COLOR = '%23939b9f';
 export const BASE_NUMBER_HEXADECIMAL = 16;
 export const DEFAULT_ICON_SIZE = 50;
 export const DEFAULT_ANCHOR_SHIFT = 25;
--- src/app/utils/styles.ts
+++ src/app/utils/styles.ts
@@ -6,70 +6,70 @@ export interface AgencyStyle {
 export const AGENCY_TO_STYLE: Map<string, AgencyStyle> = new Map();
 
 AGENCY_TO_STYLE.set('stm', {
-    backgroundColor: '#00a9e8',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%2300a9e8',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('stl', {
-    backgroundColor: '#0b1263',
-    vehicleIconColor: '#f0f8ff',
+    backgroundColor: '%230b1263',
+    vehicleIconColor: '%23f0f8ff',
 });
 AGENCY_TO_STYLE.set('trains', {
-    backgroundColor: '#b90068',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23b90068',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citla', {
-    backgroundColor: '#5199a5',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%235199a5',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('mrclasso', {
-    backgroundColor: '#4aa292',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%234aa292',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('mrclm', {
-    backgroundColor: '#9b4260',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%239b4260',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citvr', {
-    backgroundColor: '#e2a18d',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23e2a18d',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citrous', {
-    backgroundColor: '#f1e2cf',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23f1e2cf',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citlr', {
-    backgroundColor: '#daecec',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23daecec',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('lrrs', {
-    backgroundColor: '#f1e2cf',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23f1e2cf',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citpi', {
-    backgroundColor: '#6f5874',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%236f5874',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citso', {
-    backgroundColor: '#b7d8cf',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23b7d8cf',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citsv', {
-    backgroundColor: '#2b66a0',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%232b66a0',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('omitsju', {
-    backgroundColor: '#d4736a',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23d4736a',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('citcrc', {
-    backgroundColor: '#eff2dd',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23eff2dd',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('cithsl', {
-    backgroundColor: '#fcf4c5',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23fcf4c5',
+    vehicleIconColor: '%23000000',
 });
 AGENCY_TO_STYLE.set('rtl', {
-    backgroundColor: '#921d2f',
-    vehicleIconColor: '#000000',
+    backgroundColor: '%23921d2f',
+    vehicleIconColor: '%23000000',
 });
